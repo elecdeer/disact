ea3453832f8191079de0e8b9399146ccd88a0351
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-07-28 23:52:48 +0900

    test: 実行順のテストを追加

--- packages/disact/src/jsx/context.test.tsx
+++ packages/disact/src/jsx/context.test.tsx
@@ -3,6 +3,7 @@
 import { describe, expect, test } from "vitest";
 import { createContext, useContext } from "./context";
 import { createRenderer } from "./renderer";
+import type { FunctionComponent } from "./jsx-internal";
 
 describe("context", () => {
 	test("defaultValue", async () => {
@@ -89,5 +90,49 @@ describe("context", () => {
 				},
 			],
 		]);
+	});
+
+	test("separated2", async () => {
+		const Context = createContext("initial");
+
+		const contextReferenceOrder: string[] = [];
+
+		const Component: FunctionComponent<{
+			wait?: number;
+			id: string;
+		}> = async ({ wait = 0, id }) => {
+			await new Promise((resolve) => setTimeout(resolve, wait));
+			contextReferenceOrder.push(id);
+			const value = useContext<string>(Context);
+			return <h1>{value}</h1>;
+		};
+
+		const Wrapper = () => {
+			return (
+				<>
+					<Context value="AAA">
+						<Component id={"A"} wait={50} />
+					</Context>
+					<Context value="BBB">
+						<Component id={"B"} wait={20} />
+						<Context value="CCC">
+							<Component id={"C"} />
+						</Context>
+					</Context>
+				</>
+			);
+		};
+
+		const render = createRenderer({});
+
+		expect(await render(<Wrapper />)).toEqual([
+			{ props: { children: "AAA" }, type: "h1" },
+			[
+				{ props: { children: "BBB" }, type: "h1" },
+				{ props: { children: "CCC" }, type: "h1" },
+			],
+		]);
+
+		expect(contextReferenceOrder).toEqual(["C", "B", "A"]);
 	});
 });
